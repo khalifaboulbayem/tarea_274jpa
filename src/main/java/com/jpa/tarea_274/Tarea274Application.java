f548c66e9ad7b420f6f2c0be8d0ea9c2be057f15
khalifaboulbayem <khalifa.boulbayem.eviden@gmail.com>
2024-04-07 20:08:40 +0100

    init  commit

---
+++ src/main/java/com/jpa/tarea_274/Tarea274Application.java
@@ -0,0 +1,13 @@
+package com.jpa.tarea_274;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea274Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea274Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/tarea_274/Tarea274ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.tarea_274;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea274ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
